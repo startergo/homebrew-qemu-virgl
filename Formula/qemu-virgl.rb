28e0ce10ddeaf1fc927b8940ce69b75591547dc5
startergo <7897244+startergo@users.noreply.github.com>
2024-05-29 19:36:05 -0400

    Update qemu-virgl.rb

--- Formula/qemu-virgl.rb
+++ Formula/qemu-virgl.rb
@@ -1,10 +1,10 @@
 class QemuVirgl < Formula
   desc "Emulator for x86 and PowerPC"
-  homepage "https://www.qemu.org/"
-url "https://github.com/qemu/qemu.git", using: :git, revision: "99fc08366b06282614daeda989d2fde6ab8a707f"	  url "https://github.com/akihikodaki/qemu/tree/macos"
-  version "20211212.1"	version "20241212.1"
-  license "GPL-2.0-only"	sha256 "d0288b547a693be3913dfbe7399c8b9452691b081e0deffa39b4eaa1aff4a095"
-
+  url "https://github.com/akihikodaki/qemu/tree/macos"
+ version "20241212.1"
+ sha256 "d0288b547a693be3913dfbe7399c8b9452691b081e0deffa39b4eaa1aff4a095"
+ license "GPL-2.0-only"
+  
   depends_on "libtool" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
