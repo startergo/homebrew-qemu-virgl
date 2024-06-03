c78d8ce9e3f94b4f5656fc5deb2bfba20f6693d9
startergo <7897244+startergo@users.noreply.github.com>
2024-06-03 13:24:52 -0400

    Update virglrenderer.rb

--- Formula/virglrenderer.rb
+++ Formula/virglrenderer.rb
@@ -1,7 +1,7 @@
 class Virglrenderer < Formula
   desc "VirGL virtual OpenGL renderer"
   homepage "https://gitlab.freedesktop.org/virgl/virglrenderer"
-  url "https://gitlab.freedesktop.org/virgl/virglrenderer.git", revision: "453017e32ace65fa2f9c908bd5a9721f65fbf2a2"
+  url "https://github.com/akihikodaki/virglrenderer/tree/macos",
   version "20211212.1"
   license "MIT"
 
@@ -12,10 +12,6 @@ class Virglrenderer < Formula
   depends_on "startergo/qemu-virgl/libepoxy-angle"
 
   # waiting for upstreaming of https://github.com/akihikodaki/virglrenderer/tree/macos
-  patch :p1 do
-    url "https://raw.githubusercontent.com/startergo/homebrew-qemu-virgl/d8e807a58717d551ecb73a6e721e49559cec1a3d/Patches/virglrenderer-v04.diff"
-    sha256 "cb9e2ea4d73cd99375bd9fc9a008f4d7e53249a6259d63ff8f367a08c4fd8b9c"
-  end
 
   def install
     mkdir "build" do
