2ab1125308345ee7a236fed6b48cce726c7d71cb
startergo <7897244+startergo@users.noreply.github.com>
2025-03-09 13:38:10 -0400

    upd

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -9,16 +9,29 @@ class Libangle < Formula
     angle_dir = "/Users/macbookpro/Documents/angle/angle"
 
     # Install include files
-    include.install Dir["#{angle_dir}/include/*"]
+    include.install Dir["#{angle_dir}/include/CL/*"]
+    include.install Dir["#{angle_dir}/include/EGL/*"]
+    include.install Dir["#{angle_dir}/include/GLES/*"]
+    include.install Dir["#{angle_dir}/include/GLES2/*"]
+    include.install Dir["#{angle_dir}/include/GLES3/*"]
+    include.install Dir["#{angle_dir}/include/GLSLANG/*"]
+    include.install Dir["#{angle_dir}/include/KHR/*"]
+    include.install Dir["#{angle_dir}/include/WGL/*"]
+    include.install "#{angle_dir}/include/angle_cl.h"
+    include.install "#{angle_dir}/include/angle_gl.h"
+    include.install "#{angle_dir}/include/angle_windowsstore.h"
+    include.install "#{angle_dir}/include/export.h"
+    include.install Dir["#{angle_dir}/include/platform/*"]
+    include.install Dir["#{angle_dir}/include/vulkan/*"]
 
     # Install libraries from out/Release
     lib.install "#{angle_dir}/out/Release/libEGL.dylib"
     lib.install "#{angle_dir}/out/Release/libGLESv2.dylib"
 
     # Install other files
-    prefix.install "AUTHORS"
-    prefix.install "LICENSE"
-    prefix.install "README.md"
+    prefix.install "#{angle_dir}/AUTHORS"
+    prefix.install "#{angle_dir}/LICENSE"
+    prefix.install "#{angle_dir}/README.md"
   end
 
   test do
