962af3a286b226e0c730f27a0da4532552294d57
startergo <7897244+startergo@users.noreply.github.com>
2025-03-09 13:36:30 -0400

    upd

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -1,37 +1,24 @@
 class Libangle < Formula
   desc "Conformant OpenGL ES implementation for Windows, Mac, Linux, iOS, and Android"
   homepage "https://github.com/google/angle"
-  url "https://github.com/google/angle.git", using: :git, revision: "df0f7133799ca6aa0d31802b22d919c6197051cf"
+  url "https://github.com/google/angle.git", revision: "df0f7133799ca6aa0d31802b22d919c6197051cf"
   version "20250309.1"
   license "BSD-3-Clause"
 
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
-  depends_on "python@3.9" => :build
-
-  resource "depot_tools" do
-    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git", using: :git, revision: "dc86a4b9044f9243886ca0da0c1753820ac51f45"
-  end
-
   def install
-    resource("depot_tools").stage(buildpath/"depot_tools")
-    ENV.prepend_path "PATH", buildpath/"depot_tools"
+    angle_dir = "/Users/macbookpro/Documents/angle/angle"
 
-    # Bootstrap the environment
-    system "python3", "scripts/bootstrap.py"
-    
-    # Sync dependencies
-    system "gclient", "sync"
-    
-    # Generate build files
-    system "gn", "gen", "--args=is_debug=false out/Release"
-    
-    # Build the project
-    system "autoninja", "-C", "out/Release"
+    # Install include files
+    include.install Dir["#{angle_dir}/include/*"]
 
-    # Install the built libraries
-    lib.install Dir["out/Release/*.dylib"]
-    include.install Dir["include/*"]
+    # Install libraries from out/Release
+    lib.install "#{angle_dir}/out/Release/libEGL.dylib"
+    lib.install "#{angle_dir}/out/Release/libGLESv2.dylib"
+
+    # Install other files
+    prefix.install "AUTHORS"
+    prefix.install "LICENSE"
+    prefix.install "README.md"
   end
 
   test do
