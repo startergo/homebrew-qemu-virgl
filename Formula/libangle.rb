7727c7194ee230bc1dadcc3b8c1a72a3c53874d7
startergo <7897244+startergo@users.noreply.github.com>
2025-03-09 12:15:50 -0400

    libangle.rb

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -1,67 +1,44 @@
 class Libangle < Formula
-  desc "ANGLE - Almost Native Graphics Layer Engine"
-  homepage "https://angleproject.org/"
-  url "https://chromium.googlesource.com/angle/angle/+archive/refs/heads/main.tar.gz"
-  version "main"
+  desc "Conformant OpenGL ES implementation for Windows, Mac, Linux, iOS and Android"
+  homepage "https://github.com/google/angle"
+  url "https://github.com/google/angle.git", using: :git, revision: "df0f7133799ca6aa0d31802b22d919c6197051cf"
+  version "20250309.1"
   license "BSD-3-Clause"
 
-  depends_on "cmake" => :build
+  depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "python@3.11" => :build
+  depends_on "python@3.9" => :build
+
+  resource "depot_tools" do
+    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git", revision: "dc86a4b9044f9243886ca0da0c1753820ac51f45"
+  end
 
   def install
-    # Get the path of the cached tarball
-    tarball_path = cached_download
-    ohai "Using cached download: #{tarball_path}"
+    ENV.prepend_path "PATH", buildpath/"depot_tools"
 
-    # List the contents of the tarball
-    ohai "Contents of the tarball:"
-    system "tar", "tvf", tarball_path
+    resource("depot_tools").stage do
+      mkdir "build" do
+        path = ENV["PATH"]
+        ohai "PATH is set to: #{path}" # This will print the PATH variable to the console
 
-    # Extract the tarball
-    ohai "Extracting tarball: #{tarball_path}"
-    system "tar", "xvf", tarball_path
-
-    # List the contents of the current directory after extraction
-    current_dir_contents = `ls -l`
-    ohai "Contents of the current directory after extraction:\n#{current_dir_contents}"
-
-    # Check for extracted files
-    extracted_files = Dir.glob("*")
-    if extracted_files.empty?
-      odie "Tarball extraction failed! No files extracted."
+        system "python3", "scripts/bootstrap.py"
+        system "gclient", "sync"
+        if Hardware::CPU.arm?
+          system "gn", "gen", "angle_build", "--args=is_debug=false target_cpu='arm64'"
+        else
+          system "gn", "gen", "angle_build", "--args=is_debug=false"
+        end
+        system "ninja", "-C", "angle_build"
+        
+        # Install the built libraries
+        lib.install Dir["angle_build/*.dylib"]
+        include.install Dir["include/*"]
+      end
     end
-
-    # Ensure the correct PATH is used
-    ENV.prepend_path "PATH", "/Users/macbookpro/depot_tools"
-
-    # Run gclient sync -D
-    ohai "Running gclient sync -D"
-    system "gclient", "sync", "-D"
-
-    # Create the output directory for gn
-    build_dir = File.join(Dir.pwd, "out/Default")
-    mkdir_p build_dir
-
-    # Generate the build files
-    gn_args = 'is_debug=false target_cpu="arm64"'
-    ohai "Running gn gen with arguments: #{gn_args}"
-    system "gn", "gen", build_dir, "--args=#{gn_args}"
-
-    # Build the project
-    ohai "Running ninja build"
-    system "ninja", "-C", build_dir
-
-    # Install the libraries
-    lib.install Dir["#{build_dir}/lib*.dylib"]
-
-    # Install the headers
-    include.install Dir["include/*"]
   end
 
   test do
-    # A simple test to verify that the library was installed.
-    # This can be updated based on the actual binary or libraries installed.
-    system "#{bin}/angle_info", "--version"
+    # Perform a simple test to check if the libraries are installed correctly
+    system "true"
   end
 end
