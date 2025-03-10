e5b768d46a06e0afa2c3bf52ff3173faa51fc71f
startergo <7897244+startergo@users.noreply.github.com>
2025-03-09 20:58:41 -0400

    u

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -1,7 +1,7 @@
 class Libangle < Formula
   desc "Conformant OpenGL ES implementation for Windows, Mac, Linux, iOS and Android"
   homepage "https://github.com/google/angle"
-  url "https://github.com/google/angle.git", using: :git, revision: "df0f7133799ca6aa0d31802b22d919c6197051cf"
+  url "https://chromium.googlesource.com/angle/angle.git", using: :git, revision: "df0f7133799ca6aa0d31802b22d919c6197051cf"
   version "20250309.1"
   license "BSD-3-Clause"
 
@@ -11,63 +11,39 @@ class Libangle < Formula
     sha256 cellar: :any, monterey: "748d93eeabbc36f740e84338393deea0167c49da70e069708c54f5767003d12f"
   end
 
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
   depends_on "python@3.9" => :build
 
   resource "depot_tools" do
-    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git", revision: "dc86a4b9044f9243886ca0da0c1753820ac51f45"
+    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
   end
 
   def install
-    # Use Homebrew's Python 3.9
-    ENV.prepend_path "PATH", Formula["python@3.9"].opt_bin
-
-    # Stage and check out the depot_tools resource
+    # Clone the depot_tools repository
     resource("depot_tools").stage do
-      ENV.prepend_path "PATH", Dir.pwd
-
-      # Full path to python3.9
-      python3_9_path = "#{Formula["python@3.9"].opt_bin}/python3.9"
-
-      # Diagnostic step: Check if python3.9 exists
-      system "ls", "-l", python3_9_path
-
-      # Diagnostic step: Check the contents of the depot_tools directory
-      system "ls", "-l"
-
-      # Configure gclient
-      system "gclient", "config", "--name", "src", "https://chromium.googlesource.com/angle/angle.git"
-
-      # Sync the ANGLE repository using gclient
-      system "gclient", "sync"
+      depot_tools_path = Pathname.pwd
+      ENV.prepend_path "PATH", depot_tools_path
     end
 
-    # Diagnostic step: Check if the ANGLE repository has been cloned and its contents
-    angle_repo_path = "src"  # Assuming gclient sync clones ANGLE into the 'src' directory
-    system "ls", "-l", angle_repo_path
-    system "ls", "-lR", "#{angle_repo_path}"
+    # Navigate to the downloaded angle source directory
+    cd "angle" do
+      # Run bootstrap script
+      system "python3", "scripts/bootstrap.py"
 
-    # Create a build directory and generate build files with GN
-    mkdir "build" do
-      if Hardware::CPU.arm?
-        system "gn gen --args=use_custom_libcxx=false target_cpu=\"arm64\" treat_warnings_as_errors=false ../src/angle_build 2>&1 | tee gn_gen_output.txt"
-      else
-        system "gn gen --args=use_custom_libcxx=false treat_warnings_as_errors=false ../src/angle_build 2>&1 | tee gn_gen_output.txt"
-      end
-      # Diagnostic step: Check the contents of the build directory and the output log before building
-      system "ls", "-l", "../src/angle_build"
-      system "cat", "gn_gen_output.txt"
-      
-      # Build ANGLE using Ninja
-      system "ninja", "-C", "../src/angle_build"
+      # Sync the repository
+      system "gclient", "sync"
+
+      # Generate build files with GN
+      system "gn", "gen", "--args=is_debug=false out/Release"
+
+      # Build ANGLE using autoninja
+      system "autoninja", "-C", "out/Release"
       
       # Install the built libraries and headers
-      lib.install "../src/angle_build/libabsl.dylib"
-      lib.install "../src/angle_build/libEGL.dylib"
-      lib.install "../src/angle_build/libGLESv2.dylib"
-      lib.install "../src/angle_build/libchrome_zlib.dylib"
-      include.install Pathname.glob("include/*")
+      lib.install "out/Release/libabsl.dylib"
+      lib.install "out/Release/libEGL.dylib"
+      lib.install "out/Release/libGLESv2.dylib"
+      lib.install "out/Release/libchrome_zlib.dylib"
+      include.install Dir["include/*"]
     end
   end
 
