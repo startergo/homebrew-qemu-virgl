57e41cc91c2e4e0d2feaa90ffb7c8b8a15166ca1
startergo <7897244+startergo@users.noreply.github.com>
2025-03-10 13:47:55 -0400

    u

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -59,7 +59,7 @@ class Libangle < Formula
           system "git", "fetch", "--unshallow"
         end
         system "python3", "build/gen.py"
-        system "ninja", "-C", "out"
+        system "ninja", "-j", "4", "-C", "out"  # Limit to 4 parallel jobs
         bin.install "out/gn"
       end
     end
